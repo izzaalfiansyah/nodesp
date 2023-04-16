c3e6d37b779cfd028964197a016412df5c03d535
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-04-17 03:18:30 +0700

    add mqtt client

--- iot/micropython/main.py
+++ iot/micropython/main.py
@@ -2,10 +2,53 @@ import machine
 import time
 import dht
 import ujson as json
+import network
+import ubinascii
+import umqtt
 
-d = dht.DHT11(machine.Pin(3))
+wifi_ssid = 'Poltek Cabang Puri'
+wifi_pass = 'gakngerti'
 
-while True:
+mqtt_client_id = ubinascii.hexlify(machine.unique_id())
+mqtt_client = 0
+mqtt_server = '192.168.1.16'
+
+#d = dht.DHT11(machine.Pin(3))
+loop = True
+count = 0;
+
+def connect_wifi():
+  sta_if = network.WLAN(network.STA_IF)
+  sta_if.active(True)
+  sta_if.connect(wifi_ssid, wifi_pass)
+
+  while sta_if.isconnected() == False:
+    print('Gagal menyambung ke jaringan. Mencoba ulang...')
+    time.sleep(3)
+
+  print('Berhasil tersambung dengan jaringan.')
+  print(sta_if.ifconfig())
+
+def connect_mqtt():
+  connected = False
+  
+  while not connected:
+    try:
+      global mqtt_client
+      
+      mqtt_client = umqtt.client = MQTTClient(mqtt_client_id, mqtt_server, keepalive=30)
+      mqtt_client.connect()
+      
+      print("Berhasil tersambung dengan MQTT Broker.")
+      connected = True
+    except:
+      print('Gagal menyambung ke MQTT Broker. Mencoba ulang...')
+      time.sleep(3)
+
+connect_wifi()
+connect_mqtt()
+
+while loop:
   try:
     d.measure()
     
@@ -23,7 +66,15 @@ while True:
   data_json = json.dumps(data)
   print(data_json)
   
-  print("Temperatur   : %s" % temperatur)
-  print("Kelembaban   : %s" % kelembaban)
+  if (count <= 0):
+    try:
+      mqtt_client.publish('/keadaan', data_json)
+      print("Mempublish data")
+      count = 60
+    except:
+      print("Gagal mempublish data.")
+  
+  count -= 1
   
   time.sleep(1)
+
